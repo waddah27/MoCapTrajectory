b472b68c82bb83feb80edfcc849c26c296668865
Waddahkh Ali <waddahkh.ali94@gmail.com>
2023-03-13 00:30:26 +0300

    completed the PositionalEncoder block

--- TimeSeriesTransformer/PositionalEncoder.py
+++ TimeSeriesTransformer/PositionalEncoder.py
@@ -24,4 +24,16 @@ class PositionalEncoder(nn.Module):
         position = torch.arange(max_seq_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0,d_model,2) * (-math.log(10000.0)/d_model))
         pe = torch.zeros(max_seq_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x : Tensor, shape [batch_size, enc_seq_len, dim_val] or
+                [enc_seq_len, batch_size, dim_val]
+        """
+        x = x + self.pe[:x.size(self.x_dim)]
+        return self.dropout(x)
+    
     
