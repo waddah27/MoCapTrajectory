cf62e522783bba7c737a5ebec5836b0919151555
Waddahkh Ali <waddahkh.ali94@gmail.com>
2023-04-13 03:39:45 +0300

    enhanced the MultiHeadAttention class

--- Transformer_utils/MultiHeadAttention.py
+++ Transformer_utils/MultiHeadAttention.py
@@ -4,25 +4,28 @@ import torch.nn.functional as f
 from torch import nn
 
 
-def scaled_dot_product_attention(query: Tensor, key: Tensor, value: Tensor) -> Tensor:
-    '''
-    Q, K, and V (query, key, and value arrays) are batches of matrices, 
-    each with shape (batch_size, sequence_length, num_features). 
-    Batch matrix multiplication is only performed over the last two dimensions.
-    '''
-    attention = query.bmm(key.transpose(1,2))
-    scale = query.size(-1) ** 0.5
-    softmax = f.softmax(attention / scale, dim=-1)
-    return softmax.bmm(value)
-
 class HeadAttention(nn.Module):
+    '''
+    Each HeadAttention is a scaled dot product attention of query, key and value
+    '''
     def __init__(self, dim_in: int, dim_q: int, dim_k: int) -> None:
         super().__init__()
         self.q = nn.Linear(dim_in, dim_q)
         self.k = nn.Linear(dim_in, dim_k)
         self.v = nn.Linear(dim_in, dim_k)
+        
+    def scaled_dot_product_attention(self, query: Tensor, key: Tensor, value: Tensor) -> Tensor:
+        '''
+        Q, K, and V (query, key, and value arrays) are batches of matrices, 
+        each with shape (batch_size, sequence_length, num_features). 
+        Batch matrix multiplication is only performed over the last two dimensions.
+        '''
+        attention = query.bmm(key.transpose(1,2))
+        scale = query.size(-1) ** 0.5
+        softmax = f.softmax(attention / scale, dim=-1)
+        return softmax.bmm(value)
     def forward(self, query: Tensor, key: Tensor, value: Tensor) -> Tensor:
-        return scaled_dot_product_attention(self.q(query), self.k(key), self.v(value))
+        return self.scaled_dot_product_attention(self.q(query), self.k(key), self.v(value))
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads: int, dim_in: int, dim_q: int, dim_k: int) -> None:
