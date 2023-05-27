0e120a67b106213a937771f25c3930ef7bdf8981
Waddahkh Ali <waddahkh.ali94@gmail.com>
2023-05-27 16:15:34 +0300

    cloned dmpling repo to use DMPs for my trajectory data

---
+++ DMPs/test_dmps.py
@@ -0,0 +1,12 @@
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy as np
+import c3d
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+
+sys.path.append("../")
+from dmpling.dmp import DMP
